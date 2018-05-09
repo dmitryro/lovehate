551bca82611d93e6ea152a4e8212bda6bb0b561b
dmitryro <dmitryro@gmail.com>
2018-05-08 23:58:12 -0400

    Update callbacks.py

--- custom/users/callbacks.py
+++ custom/users/callbacks.py
@@ -29,9 +29,75 @@ from custom.utils.models import Logger
 from custom.users.models import Profile
 from custom.users.signals import user_needs_recovery
 from custom.users.signals import user_resend_activation
+from custom.users.signals import user_send_reset_password_link
 from custom.meta.models import ProfileMetaProp
 from custom.users.mail import Gmail
 from settings import settings
+
+@receiver(user_send_reset_password_link, sender=User)
+def reset_password_link(sender, instance, **kwargs):
+    mess = 'Please activate your account.'    
+    try:
+        activation_key = utils.create_activation_key(instance)
+        link = settings.BASE_URL+'/activate/%s'%activation_key
+        log = Logger(log='USER ACTIVATION KEY IS {}'.format(activation_key))
+        log.save()
+        user_profile = instance.profile
+        user_profile.activation_key = activation_key
+        user_profile.save()
+
+        timeNow = datetime.now()
+
+        profile = ProfileMetaProp.objects.get(pk=1)
+        FROM = '<strong>Любовь и Ненависть'
+        USER = profile.user_name
+        PASSWORD = profile.password
+        PORT = profile.smtp_port
+        SERVER = profile.smtp_server
+        TO = instance.profile.email
+        SUBJECT = 'Восстановление доступа'
+          
+        MESSAGE = MIMEMultipart('alternative')
+        MESSAGE['subject'] = SUBJECT
+        MESSAGE['To'] = TO
+        MESSAGE['From'] = "{}".format(FROM)
+        MESSAGE.preamble = """
+                Your mail reader does not support the report format.
+                Please visit us <a href="http://www.divorcesus.com">online</a>!"""
+ 
+        f = codecs.open("templates/activate_inline.html", 'r')
+        mess = str(f.read())
+        mess = str.replace(mess, '[greeting]', 'Приветствуем Вас на ЛХ,')
+        mess = str.replace(mess, '[greeting_statement]', 'Нажмите на кнопку ниже для восстановления доступа.')
+        mess = str.replace(mess, '[greeting_link]','Восстановить Пароль')
+        mess = str.replace(mess, '[greeting_sent]', 'Это сообщение было послано на адрес')
+        mess = str.replace(mess, '[greeting_global_link]', 'Любовь и Ненависть')
+        mess = str.replace(mess, '[greeting_locale]', 'Москва, Российская Федерация')
+        mess = str.replace(mess, '[First Name]', instance.username)
+        mess = str.replace(mess, '[message]', 'Восстановление Доступа')
+        mess = str.replace(mess,'email_address@email.com', instance.profile.email)
+        mess = str.replace(mess,'[link]', link)
+
+
+        HTML_BODY  = MIMEText(mess.encode('utf-8'), 'html','utf-8')
+
+        MESSAGE.attach(HTML_BODY)
+        msg = MESSAGE.as_string()
+
+        server = smtplib.SMTP(SERVER+':'+PORT)
+        server.ehlo()
+        server.starttls()
+        server.login(USER, PASSWORD)
+        server.sendmail(FROM, TO, msg)
+        server.quit()
+        log = Logger(log='MESSAGE SENT WAS {}'.format(msg))
+        log.save()
+        instance.profile.activation_key = activation_key
+        instance.profile.save()
+        instance.save()
+    except Exception as R:
+        log = Logger(log='Failed resetting {}'.format(str(R)))
+        log.save()
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
